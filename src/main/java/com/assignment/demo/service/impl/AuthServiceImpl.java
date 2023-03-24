b84a68735cbc75ab1d477ef18b738359ff3eaa84
Manoj Yadav <manoj.yadav.sde@gmail.com>
2023-03-24 15:42:51 +0530

    Added register logic in register of Auth service

--- src/main/java/com/assignment/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/assignment/demo/service/impl/AuthServiceImpl.java
@@ -1,25 +1,42 @@
 package com.assignment.demo.service.impl;
 
+import com.assignment.demo.entity.Role;
+import com.assignment.demo.entity.User;
 import com.assignment.demo.payload.LoginDto;
 import com.assignment.demo.payload.RegisterDto;
+import com.assignment.demo.repository.RoleRepository;
+import com.assignment.demo.repository.UserRepository;
 import com.assignment.demo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Autowired
     public AuthServiceImpl(
-            AuthenticationManager authenticationManager
+            AuthenticationManager authenticationManager,
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder
     ) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -40,6 +57,20 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String register(RegisterDto registerDto) {
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setEmail(registerDto.getEmail());
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER").orElse(null);
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
         return "user registered succesfully";
     }
 }
