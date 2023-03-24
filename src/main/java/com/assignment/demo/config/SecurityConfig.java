0ea75c884fcfdbd2ac828d7fc2d3b388a3140d6a
Manoj Yadav <manoj.yadav.sde@gmail.com>
2023-03-24 15:32:00 +0530

    implemented authentication in login of Auth service

--- src/main/java/com/assignment/demo/config/SecurityConfig.java
+++ src/main/java/com/assignment/demo/config/SecurityConfig.java
@@ -2,13 +2,22 @@ package com.assignment.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,5 +29,10 @@ public class SecurityConfig {
                 );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/assignment/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/assignment/demo/service/impl/AuthServiceImpl.java
@@ -3,13 +3,39 @@ package com.assignment.demo.service.impl;
 import com.assignment.demo.payload.LoginDto;
 import com.assignment.demo.payload.RegisterDto;
 import com.assignment.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthServiceImpl(
+            AuthenticationManager authenticationManager
+    ) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @Override
     public String login(LoginDto loginDto) {
-        return "manoj";
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsernameOrEmail(),
+                        loginDto.getPassword()
+                ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = "manoj";
+
+        return token;
     }
 
     @Override
